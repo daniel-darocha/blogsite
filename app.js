ca7643adf19361e8519305aa4fb4f8bc81b7333e
Daniel Da Rocha <daniel@Daniels-MBP.hitronhub.home>
2022-03-16 19:50:11 -0400

    add new pages with updated partials. linked routes

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ app.get("/", function(req,res){
   res.render('home',{homeStartingContent: homeStartingContent});
 });
 
+app.get("/compose", function(req,res){
+
+  res.render('compose',{});
+});
+
+app.get("/contact", function(req,res){
+
+  res.render('contact',{contactContent: contactContent});
+});
+
+app.get("/about", function(req,res){
+
+  res.render('about',{aboutContent: aboutContent});
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
