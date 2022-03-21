49f0c250322190555b2fea874c7692bbab6db724
Daniel Da Rocha <daniel@Daniels-MacBook-Pro.local>
2022-03-20 23:48:27 -0400

    add some route params and EJS Loop on home for /compose content

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { json } = require("body-parser");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,15 +18,18 @@ app.use(express.static("public"));
 
 // Global Variables
 
-const posts = [];
+var posts = [];
 
 
 // ---- Get Routes
-
 app.get("/", function(req,res){
-
-  res.render('home',{homeStartingContent: homeStartingContent});
+  res.render('home',{homeStartingContent: homeStartingContent, posts: posts});
 });
+
+app.get("/posts/:postID", function(req,res){
+  console.log(req.params.postID)
+
+})
 
 app.get("/compose", function(req,res){
 
@@ -52,7 +56,6 @@ postBody: req.body.content
 };
 
 posts.push(post);
-console.log(posts)
 
 res.redirect("/");
 });
