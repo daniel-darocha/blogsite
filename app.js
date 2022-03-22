42d5586bfcbf4fba68ba0ceb440a7394bc291144
Daniel Da Rocha <daniel@daniels-mbp.localdomain>
2022-03-22 17:00:07 -0400

    update on param route. Render for /posts/: route now available if route matches post name

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { json } = require("body-parser");
+const _ = require('lodash');
+const array = require('lodash/array');
+const object = require('lodash/fp/object');
+const { trimEnd } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
 // Global Variables
 
 var posts = [];
@@ -27,7 +32,14 @@ app.get("/", function(req,res){
 });
 
 app.get("/posts/:postID", function(req,res){
-  console.log(req.params.postID)
+  var postName = _.lowerCase(req.params.postID);
+  
+  posts.forEach(function(p){
+    if (_.lowerCase(p.postTitle) === postName) {
+      res.render('post', {title: p.postTitle, body: p.postBody});
+    }
+  });
+
 
 })
 
